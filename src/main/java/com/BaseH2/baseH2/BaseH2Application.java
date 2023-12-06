d995f4fdaa677aa5e251d5dc27443491ef89ece1
danielamoreno <lauritamobu@gmail.com>
2023-12-05 21:21:53 -0500

    Creacion proyecto base de datos H2

---
+++ src/main/java/com/BaseH2/baseH2/BaseH2Application.java
@@ -0,0 +1,13 @@
+package com.BaseH2.baseH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/BaseH2/baseH2/BaseH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BaseH2.baseH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
